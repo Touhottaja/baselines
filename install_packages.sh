92b31032c335cabf7ba8f6955e55f45670ccd76d
Touhottaja <touhottaja@icloud.com>
2024-04-09 20:21:11 +0300

    Add script for installing packages

---
+++ install_packages.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+apt_packages=(
+    # Editors
+    vim
+    ipython3
+    # Window managers
+    i3
+    i3lock
+    arandr
+    # Environments
+    python3-venv
+    # Screenshots (on i3)
+    xclip
+    maim
+    # Networking
+    curl
+    # Fun stuff
+    cowsay
+    fortune
+    # Hacking/CTF stuff
+    nmap
+    ffuf
+    gobuster
+)
+
+snap_packages=(
+    # Editors
+    code
+)
+
+install_packages_apt() {
+    for package in "${apt_packages[@]}"; do
+        sudo apt install -y "$package"
+    done
+}
+
+install_packages_snap() {
+    for package in "${snap_packages[@]}"; do
+        sudo snap install --classic "$package"
+    done
+}
+
+sudo apt update
+sudo apt upgrade -y
+
+install_packages_apt
+install_packages_snap
+
