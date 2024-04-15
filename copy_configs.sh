17ef34db2d4fe87fd4cb91fce816dd6bec61da1f
Touhottaja <touhottaja@icloud.com>
2024-04-15 19:44:57 +0300

    Fix typo and step for creating i3status
    Fixed typo (bashrc -> .bashrc) and added a step for creating the
    i3status folder under ~/.config

--- copy_configs.sh
+++ copy_configs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-cp .bashrc ~/bashrc
+cp .bashrc ~/.bashrc
 cp .gitconfig ~/.gitconfig
 cp config.i3 ~/.config/i3/config
+mkdir ~/.config/i3status/
 cp config.i3status ~/.config/i3status/config
 
